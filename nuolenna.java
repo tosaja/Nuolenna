7e3d100cc149681f8064dcb97ebddff6a9e98f16
Tommi Jauhiainen <tommi.jauhiainen@helsinki.fi>
2026-09-16 15:48:28 +0200

    I made a separate method for creating cuneiform from a string

--- nuolenna.java
+++ nuolenna.java
@@ -60,141 +60,9 @@ class nuolenna {
 // Logograms are written in capitals, but the signs are the same
                 line = line.toLowerCase(Locale.ROOT);
 				
-				
-				String[] sanat = line.split(" ");
-				for (String sana : sanat) {
-// REPETITION '(' GRAPHEME ')'
-					if (sana.matches("^[1-90][1-90]*\\(.*\\)$")) {
-						String merkki = sana.replaceAll("^[1-90][1-90]*\\(", "");
-						merkki = merkki.replaceAll("\\)$", "");
-						int maara = Integer.valueOf(sana.replaceAll("\\(.*$", ""));
-						sana = merkki;
-						if (maara > 10) {
-							maara = 10;
-						}
-						while (maara > 1) {
-							sana = sana + " " + merkki;
-							maara = maara - 1;
-						}
-					}
-										
-// $-sign means that the reading is uncertain (the sign is still certain) so we just remove all dollar signs
-					sana = sana.replaceAll("[\\$]", "");
-// some complicated combination characters have their own sign in UTF, transformations here before removing pipes
-					sana = sana.replaceAll("gad\\&gad\\.gar\\&gar", "kinda");
-					sana = sana.replaceAll("bu\\&bu\\.ab", "sirsir");
-					sana = sana.replaceAll("tur\\&tur\\.za\\&za", "zizna");
-					sana = sana.replaceAll("še\\&še\\.tab\\&tab.gar\\&gar", "garadin₃");
-// "Signs which have the special subscript ₓ must be qualified in ATF by placing the sign name in parentheses immediately after the sign value"
-// http://oracc.museum.upenn.edu/doc/help/editinginatf/primer/inlinetutorial/index.html
-					if (sana.matches(".*[\\.-][^\\.-]*ₓ\\(.*\\).*")) {
-						while (sana.matches(".*[\\.-][^\\.-]*ₓ\\(.*\\).*")) {
-							sana = sana.replaceAll("(.*[\\.-])([^\\.-]*ₓ\\()([^\\)]*)(\\))(.*)", "$1$3$5");
-						}
-					}
-					if (sana.matches(".*ₓ\\(.*\\).*")) {
-						while (sana.matches(".*ₓ\\(.*\\).*")) {
-							sana = sana.replaceAll("(.*ₓ\\()([^\\)]*)(\\))(.*)", "$2$4");
-						}
-					}
-					
-					
-// old or more precise readings can be within parenthesis straight after the sign. We just remove the parenthesis and what is inside them
-// first we handle "xxx(|...|)"
-					if (sana.matches(".*[^\\|\\&]\\(\\|[^\\|]*\\|\\).*")) {
-						while (sana.matches(".*[^\\|\\&]\\(\\|[^\\|]*\\|\\).*")) {
-							sana = sana.replaceAll("(.*[^\\|\\&])(\\(\\|[^\\|]*\\|\\))(.*)", "$1$3");
-						}
-					}
-										
-// then we handle "|...|(...)"
-					if (sana.matches(".*\\|[^\\|]*\\|\\(.*\\).*")) {
-						while (sana.matches(".*\\|[^\\|]*\\|\\(.*\\).*")) {
-							sana = sana.replaceAll("(.*\\|[^\\|]*\\|)(\\(.*\\))(.*)", "$1$3");
-						}
-					}
-										
-// then we remove the more general case
-					if (sana.matches(".*[\\.-][^\\.-]*[^\\|\\&]\\(.*\\).*")) {
-						while (sana.matches(".*[\\.-][^\\.-]*[^\\|\\&]\\(.*\\).*")) {
-							sana = sana.replaceAll("(.*[\\.-][^\\.-]*[^\\|\\&])(\\(.*\\))(.*)", "$1$3");
-						}
-					}
-					
-					if (sana.matches(".*[^\\|\\&]\\(.*\\).*")) {
-						while (sana.matches(".*[^\\|\\&]\\([^\\(\\)]*\\).*")) {
-							sana = sana.replaceAll("(.*[^\\|\\&])(\\([^\\(\\)]*\\))(.*)","$1$3");
-						}
-					}
-					
-					
-
-// combination characters are inside pipes, but they are indicated also by combining markers, so we check markers and remove pipes
-					sana = sana.replaceAll("\\|", "");
-// Logograms separated internally by dots (e.g., GIR₂.TAB). If they are inside (...) they are not removed yet.
-					if (!sana.matches(".*\\(.*\\..*\\).*")) {
-						sana = sana.replaceAll("[.]", " ");
-					}
-// "Phonetic complements are preceded by a + inside curly brackets (e.g., KUR{+ud} = ikšud)."
-// http://oracc.museum.upenn.edu/doc/help/editinginatf/primer/inlinetutorial/index.html
-					sana = sana.replaceAll("\\{\\+", " ");
-// joining characters are combined by + sign, we separate joining chars by replacing with whitespace
-					sana = sana.replaceAll("\\+", " ");
-					sana = sana.replaceAll("[-{}]", " ");
-// LAGAŠ = ŠIR.BUR.LA
-					sana = sana.replaceAll("lagaš ", "šir bur la ");
-
-					sana = sana.replaceAll("  *", " ");
-
-					String[] tavut = sana.split(" ");
-					for (String tavu : tavut) {
-						if (tavu.equals("x")) {
-                            writer.write("xx");
-						}
-						else if (tavu.equals("å")) {
-                            writer.write("åå");
-						}
-						else {
-	// After the characters @ and ~ there is some annotation which should no affect cuneifying, so we just remove it.
-							if (tavu.matches(".*@[19cghknrstvz]")) {
-								tavu = tavu.replaceAll("@.*", "");
-							}
-							if (tavu.matches(".*~[abcdefptyv][1234dgpt]?p?")) {
-								tavu = tavu.replaceAll("~.*", "");
-							}
-	// All numbers to one
-							if (tavu.matches("n[1-90][1-90]*") || tavu.matches("[1-90][1-90]*")) {
-								tavu = "n01";
-							}
-							
-							if (tavu.equals("1/2(iku)") || tavu.equals("1/4(iku)")) {
-								tavu = "";
-							}
-							
-							tavu = tavu.replaceAll("[\\(\\)]", "");
-							 
-							if (cuneiMap.containsKey(tavu)) {
-                                writer.write(cuneiMap.get(tavu));
-							}
-							else if ((tavu.contains("×") || tavu.contains(".")) && !tavu.contains("&")) {
-								tavu = tavu.replaceAll("[\\.]", "×");
-								String[] alatavut = tavu.split("×");
-								for (String alatavu: alatavut) {
-									if (cuneiMap.containsKey(alatavu)) {
-                                        writer.write(cuneiMap.get(alatavu));
-									}
-								}
-							}
-							else if (tavu.equals("€") || tavu.equals("o")) {
-                                writer.write("  ");
-							}
-							else {
-	//							System.out.print(tavu);
-							}
-						}
-						
-					}
-				}
+                String cuneiform = makeCuneiform(line);
+                
+                writer.write(cuneiform);
                 writer.write("\n");
 			}
 		reader.close();
@@ -204,6 +72,145 @@ class nuolenna {
 			e.printStackTrace();
 		}
 	}
+    
+    private static String makeCuneiform(String transliteration) {
+        String cuneiform = "";
+        String[] sanat = transliteration.split(" ");
+        for (String sana : sanat) {
+// REPETITION '(' GRAPHEME ')'
+            if (sana.matches("^[1-90][1-90]*\\(.*\\)$")) {
+                String merkki = sana.replaceAll("^[1-90][1-90]*\\(", "");
+                merkki = merkki.replaceAll("\\)$", "");
+                int maara = Integer.valueOf(sana.replaceAll("\\(.*$", ""));
+                sana = merkki;
+                if (maara > 10) {
+                    maara = 10;
+                }
+                while (maara > 1) {
+                    sana = sana + " " + merkki;
+                    maara = maara - 1;
+                }
+            }
+                                
+// $-sign means that the reading is uncertain (the sign is still certain) so we just remove all dollar signs
+            sana = sana.replaceAll("[\\$]", "");
+// some complicated combination characters have their own sign in UTF, transformations here before removing pipes
+            sana = sana.replaceAll("gad\\&gad\\.gar\\&gar", "kinda");
+            sana = sana.replaceAll("bu\\&bu\\.ab", "sirsir");
+            sana = sana.replaceAll("tur\\&tur\\.za\\&za", "zizna");
+            sana = sana.replaceAll("še\\&še\\.tab\\&tab.gar\\&gar", "garadin₃");
+// "Signs which have the special subscript ₓ must be qualified in ATF by placing the sign name in parentheses immediately after the sign value"
+// http://oracc.museum.upenn.edu/doc/help/editinginatf/primer/inlinetutorial/index.html
+            if (sana.matches(".*[\\.-][^\\.-]*ₓ\\(.*\\).*")) {
+                while (sana.matches(".*[\\.-][^\\.-]*ₓ\\(.*\\).*")) {
+                    sana = sana.replaceAll("(.*[\\.-])([^\\.-]*ₓ\\()([^\\)]*)(\\))(.*)", "$1$3$5");
+                }
+            }
+            if (sana.matches(".*ₓ\\(.*\\).*")) {
+                while (sana.matches(".*ₓ\\(.*\\).*")) {
+                    sana = sana.replaceAll("(.*ₓ\\()([^\\)]*)(\\))(.*)", "$2$4");
+                }
+            }
+            
+            
+// old or more precise readings can be within parenthesis straight after the sign. We just remove the parenthesis and what is inside them
+// first we handle "xxx(|...|)"
+            if (sana.matches(".*[^\\|\\&]\\(\\|[^\\|]*\\|\\).*")) {
+                while (sana.matches(".*[^\\|\\&]\\(\\|[^\\|]*\\|\\).*")) {
+                    sana = sana.replaceAll("(.*[^\\|\\&])(\\(\\|[^\\|]*\\|\\))(.*)", "$1$3");
+                }
+            }
+                                
+// then we handle "|...|(...)"
+            if (sana.matches(".*\\|[^\\|]*\\|\\(.*\\).*")) {
+                while (sana.matches(".*\\|[^\\|]*\\|\\(.*\\).*")) {
+                    sana = sana.replaceAll("(.*\\|[^\\|]*\\|)(\\(.*\\))(.*)", "$1$3");
+                }
+            }
+                                
+// then we remove the more general case
+            if (sana.matches(".*[\\.-][^\\.-]*[^\\|\\&]\\(.*\\).*")) {
+                while (sana.matches(".*[\\.-][^\\.-]*[^\\|\\&]\\(.*\\).*")) {
+                    sana = sana.replaceAll("(.*[\\.-][^\\.-]*[^\\|\\&])(\\(.*\\))(.*)", "$1$3");
+                }
+            }
+            
+            if (sana.matches(".*[^\\|\\&]\\(.*\\).*")) {
+                while (sana.matches(".*[^\\|\\&]\\([^\\(\\)]*\\).*")) {
+                    sana = sana.replaceAll("(.*[^\\|\\&])(\\([^\\(\\)]*\\))(.*)","$1$3");
+                }
+            }
+            
+            
+
+// combination characters are inside pipes, but they are indicated also by combining markers, so we check markers and remove pipes
+            sana = sana.replaceAll("\\|", "");
+// Logograms separated internally by dots (e.g., GIR₂.TAB). If they are inside (...) they are not removed yet.
+            if (!sana.matches(".*\\(.*\\..*\\).*")) {
+                sana = sana.replaceAll("[.]", " ");
+            }
+// "Phonetic complements are preceded by a + inside curly brackets (e.g., KUR{+ud} = ikšud)."
+// http://oracc.museum.upenn.edu/doc/help/editinginatf/primer/inlinetutorial/index.html
+            sana = sana.replaceAll("\\{\\+", " ");
+// joining characters are combined by + sign, we separate joining chars by replacing with whitespace
+            sana = sana.replaceAll("\\+", " ");
+            sana = sana.replaceAll("[-{}]", " ");
+// LAGAŠ = ŠIR.BUR.LA
+            sana = sana.replaceAll("lagaš ", "šir bur la ");
+
+            sana = sana.replaceAll("  *", " ");
+
+            String[] tavut = sana.split(" ");
+            for (String tavu : tavut) {
+                if (tavu.equals("x")) {
+                    cuneiform = cuneiform + ("xx");
+                }
+                else if (tavu.equals("å")) {
+                    cuneiform = cuneiform + ("åå");
+                }
+                else {
+// After the characters @ and ~ there is some annotation which should no affect cuneifying, so we just remove it.
+                    if (tavu.matches(".*@[19cghknrstvz]")) {
+                        tavu = tavu.replaceAll("@.*", "");
+                    }
+                    if (tavu.matches(".*~[abcdefptyv][1234dgpt]?p?")) {
+                        tavu = tavu.replaceAll("~.*", "");
+                    }
+// All numbers to one
+                    if (tavu.matches("n[1-90][1-90]*") || tavu.matches("[1-90][1-90]*")) {
+                        tavu = "n01";
+                    }
+                    
+                    if (tavu.equals("1/2(iku)") || tavu.equals("1/4(iku)")) {
+                        tavu = "";
+                    }
+                    
+                    tavu = tavu.replaceAll("[\\(\\)]", "");
+                     
+                    if (cuneiMap.containsKey(tavu)) {
+                        cuneiform = cuneiform + cuneiMap.get(tavu);
+                    }
+                    else if ((tavu.contains("×") || tavu.contains(".")) && !tavu.contains("&")) {
+                        tavu = tavu.replaceAll("[\\.]", "×");
+                        String[] alatavut = tavu.split("×");
+                        for (String alatavu: alatavut) {
+                            if (cuneiMap.containsKey(alatavu)) {
+                                cuneiform = cuneiform + cuneiMap.get(alatavu);
+                            }
+                        }
+                    }
+                    else if (tavu.equals("€") || tavu.equals("o")) {
+                        cuneiform = cuneiform + "  ";
+                    }
+                    else {
+//                            System.out.print(tavu);
+                    }
+                }
+                
+            }
+        }
+        return cuneiform;
+    }
 	
 	private static void loadindictionary(File file) {
 		
@@ -215,7 +222,6 @@ class nuolenna {
 			
 			while ((line = reader.readLine()) != null) {
 				String translitteraatio = line.replaceAll("\t.*", "");
-				//translitteraatio = translitteraatio.toLowerCase();
                 translitteraatio = translitteraatio.toLowerCase(Locale.ROOT);
 				String nuolenpaa = line.replaceAll(".*\t", "");
 // We'll change all combination signs to just signs following each other
